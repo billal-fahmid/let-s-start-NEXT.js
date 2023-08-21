14516ffddb8712e9d4256341598fc6e330eb9479
billal-fahmid <billalcooom@gmail.com>
2023-08-21 21:17:36 +0600

    learn basic route

---
+++ src/app/about/page.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AboutPage = () => {
+    return (
+        <div className='h-screen bg-slate-300'>
+            <h2 className='text-5xl text-orange-500 font-bold text-center p-5'>This is About page</h2>
+        </div>
+    );
+};
+
+export default AboutPage;
---
+++ src/app/login/page.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const LoginPage = () => {
+    return (
+        <div className='h-screen bg-slate-300'>
+            <h2 className='text-5xl text-orange-500 font-bold text-center p-5'>this is login page</h2>
+        </div>
+    );
+};
+
+export default LoginPage;
